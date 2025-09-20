6b58931f1436ebb0dc492d3584243ff92cbe3f42
Corey Verkouteren <waffleme@mail.com>
2025-09-19 21:11:08 -0400

    add models based on sampledata

---
+++ CollegeManager/core/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+
+class Semester(models.Model):
+    semester_id = models.CharField(max_length=6, unique=True)
+
+    def __str__(self):
+        return f"{self.semester_id})"
+
+
+class Department(models.Model):
+    department_id = models.CharField(max_length=4, unique=True)
+
+    def __str__(self):
+        return f"{self.department_id})"
+
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=4, unique=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    expected_graduation = models.ForeignKey(Semester, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.student_id})"
+
+
+class Course(models.Model):
+    course_id = models.CharField(max_length=4, unique=True)  # e.g., CS 235
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    course_num = models.IntegerField()
+    title = models.CharField(max_length=40)
+    min_hours = models.FloatField()
+    max_hours = models.FloatField()
+
+    def __str__(self):
+        return f"{self.course_id}: {self.title}"
+
+
+class Section(models.Model):
+    section_id = models.CharField(max_length=6, unique=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    section_num = models.IntegerField()
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.course} - {self.semester} (Sec {self.section_id})"
+
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.student} enrolled in {self.section}"
