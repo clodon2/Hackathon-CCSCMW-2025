a5d3dca0764e9536f2ffb8ccdac8329a99aa90eb
Corey Verkouteren <waffleme@mail.com>
2025-09-19 21:35:13 -0400

    fix settings to include core app

--- CollegeManager/CollegeManager/settings.py
+++ CollegeManager/CollegeManager/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'studentplan',
-    'conflictreport'
+    'conflictreport',
+    'core'
 ]
 
 MIDDLEWARE = [
